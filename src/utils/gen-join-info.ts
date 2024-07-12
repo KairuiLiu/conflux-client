3f43f4f8b3925cedcba44889a111117acdb8cbb4
KairuiLiu <tclkr@live.com>
2024-07-12 20:10:24 +0800

    fix: join info text

--- src/utils/gen-join-info.ts
+++ src/utils/gen-join-info.ts
@@ -9,13 +9,16 @@ export function genJoinInfo(
 ) {
   return `${user} invites you to join a conflux video meeting.
 Meeting ID: ${id.replace(/(.{1,3})/g, '$1 ').trim()}${
-  passcode !== '' && `
+    passcode !== ''
+      ? `
 Passcode: ${passcode}`
-}
+      : ''
+  }
 Meeting topic: ${title}${
-    meetingTime &&
-    `
+    meetingTime
+      ? `
 Meeting time: ${getLocalDateTime(meetingTime, true)}`
+      : ''
   }
 Meeting link: ${window.location.origin}/j/${id}`;
 }
